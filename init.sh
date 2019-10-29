8c221ee429a3192a05c8860be5cde417075cb02a
Adam Short <adamconnorshort@gmail.com>
2019-10-29 12:28:44 -0400

    Janky Docker support

--- init.sh
+++ init.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
-git submodule init
-git submodule update
-git submodule add -f https://github.com/google/googletest.git benchmark/googletest
+if [ "$1" == "DOCKER" ]; then
+    git init
+    git submodule add -f https://github.com/google/benchmark.git
+    git submodule add -f https://github.com/google/googletest.git benchmark/googletest
+
+else
+    git submodule init
+    git submodule update
+    git submodule add -f https://github.com/google/googletest.git benchmark/googletest
+fi
+
 mkdir benchmark/build
 cd benchmark/build && cmake -DCMAKE_BUILD_TYPE=Release ../
 make -j8
+
+if [ "$1" == "DOCKER" ]; then
+    cd /usr/src/app
+    make run
+fi
