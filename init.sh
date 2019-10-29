ab1b96e6607c1bf96466002ca5610b372f1f9b50
Adam Short <ashort11@vols.utk.edu>
2019-10-29 09:09:34 -0400

    fixed init script for Linux

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-git submodule add -f https://github.com/google/benchmark.git
+git submodule init
+git submodule update
 git submodule add -f https://github.com/google/googletest.git benchmark/googletest
 mkdir benchmark/build
 cd benchmark/build && cmake -DCMAKE_BUILD_TYPE=Release ../
