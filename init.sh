4aacd4d1fd95aefb0492db7480403d848cf15b01
Adam Short <adamconnorshort@gmail.com>
2019-10-29 08:37:45 -0400

    Added release build

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 git submodule add -f https://github.com/google/benchmark.git
 git submodule add -f https://github.com/google/googletest.git benchmark/googletest
 mkdir benchmark/build
-cd benchmark/build && cmake ../
+cd benchmark/build && cmake -DCMAKE_BUILD_TYPE=Release ../
 make -j8
